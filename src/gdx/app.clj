cb501c0d0dd14d9c2b249ddaf8a2ec486d967c44
Michael Sappler <michaelsappler@gmail.com>
2023-07-17 16:10:14 +0200

    Add dock icon GDX logo for mac

--- src/gdx/app.clj
+++ src/gdx/app.clj
@@ -1,17 +1,22 @@
 (ns gdx.app
-  (:require [gdx.utils :refer (set-var-root)])
+  (:require [gdx.utils :refer (set-var-root)]
+            [gdx.mac-dock-icon :as mac-dock-icon])
   (:import [com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application Lwjgl3ApplicationConfiguration]
            com.badlogic.gdx.utils.Disposable
            [com.badlogic.gdx Gdx Application]))
 
 (defn- lwjgl-config [{:keys [title width height full-screen fps]}]
+  ; TODO only do that on mac
+  ;(import 'com.badlogic.gdx.utils.SharedLibraryLoader)
+  ;when SharedLibraryLoader/isMac
+  (mac-dock-icon/set-mac-os-dock-icon)
+
   (let [config (Lwjgl3ApplicationConfiguration.)]
     (.setTitle config title)
     (if full-screen
       (.setFullscreenMode config (Lwjgl3ApplicationConfiguration/getDisplayMode))
       (.setWindowedMode config width height))
-    (when fps
-      (.setForegroundFPS config fps))
+    (.setForegroundFPS config (or fps 60))
     config))
 
 (defn create [game config]
