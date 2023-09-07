902311f6a3c3a72b45d96764a690346044104565
Michael Sappler <michaelsappler@gmail.com>
2023-09-07 14:41:50 +0200

    Remove unnecessary exclamation marks

--- src/gdl/graphics/shape_drawer.clj
+++ src/gdl/graphics/shape_drawer.clj
@@ -23,64 +23,64 @@
   (lc/dispose [_]
     (.dispose ^Texture texture)))
 
-(defn- set-color! [^Color color]
+(defn- set-color [^Color color]
   (.setColor drawer color))
 
-(defn set-line-width! [width]
+(defn set-line-width [width]
   (.setDefaultLineWidth drawer (float width)))
 
 (defmacro with-line-width [width & exprs]
   `(let [old-line-width# (.getDefaultLineWidth drawer)]
-     (set-line-width! (* ~width old-line-width#))
+     (set-line-width (* ~width old-line-width#))
      ~@exprs
-     (set-line-width! old-line-width#)))
+     (set-line-width old-line-width#)))
 
 (defn ellipse [[x y] radius-x radius-y color]
-  (set-color! color)
+  (set-color color)
   (.ellipse drawer (float x) (float y) (float radius-x) (float radius-y)))
 
 (defn filled-ellipse [[x y] radius-x radius-y color]
-  (set-color! color)
+  (set-color color)
   (.filledEllipse drawer (float x) (float y) (float radius-x) (float radius-y)))
 
 (defn circle [[x y] radius color]
-  (set-color! color)
+  (set-color color)
   (.circle drawer (float x) (float y) (float radius)))
 
 (defn filled-circle [[x y] radius color]
-  (set-color! color)
+  (set-color color)
   (.filledCircle drawer (float x) (float y) (float radius)))
 
 (defn- degree->radians [degree] ; TODO not here
   (* degree (/ (Math/PI) 180)))
 
 (defn arc [[centre-x centre-y] radius start-angle degree color]
-  (set-color! color)
+  (set-color color)
   (.arc drawer centre-x centre-y radius start-angle (degree->radians degree)))
 
 (defn sector [[centre-x centre-y] radius start-angle degree color]
-  (set-color! color)
+  (set-color color)
   (.sector drawer centre-x centre-y radius start-angle (degree->radians degree)))
 
 (defn rectangle
   ([[x y w h] color]
    (rectangle x y w h color))
   ([x y w h color]
-   (set-color! color)
+   (set-color color)
    (.rectangle drawer x y w h)))
 
 (defn filled-rectangle
   ([[x y w h] color]
    (filled-rectangle x y w h color))
   ([x y w h color]
-   (set-color! color)
+   (set-color color)
    (.filledRectangle drawer (float x) (float y) (float w) (float h))))
 
 (defn line
   ([[x y] [ex ey] color]
    (line x y ex ey color))
   ([x y ex ey color]
-   (set-color! color)
+   (set-color color)
    (.line drawer (float x) (float y) (float ex) (float ey))))
 
 (defn grid
