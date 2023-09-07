49355d2ea5b163eb339c64a9be94eb06fc7464ae
Michael Sappler <michaelsappler@gmail.com>
2023-09-07 14:43:35 +0200

    Fix compilation error

--- src/gdl/render.clj
+++ src/gdl/render.clj
@@ -7,7 +7,7 @@
 
 (defn render-with [unit-scale ^OrthographicCamera camera renderfn]
   (binding [*unit-scale* unit-scale]
-    (shape-drawer/set-line-width! *unit-scale*)
+    (shape-drawer/set-line-width *unit-scale*)
     (.setColor batch color/white) ; fix scene2d.ui.tooltip flickering
     (.setProjectionMatrix batch (.combined camera))
     (.begin batch)
