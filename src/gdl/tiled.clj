a075686bb0b51f8ebc136a15d7b71643b61d4065
Michael Sappler <michaelsappler@gmail.com>
2023-12-06 20:14:40 +0100

    Remove gdl.graphics.batch dependency from gdl.tiled

--- src/gdl/tiled.clj
+++ src/gdl/tiled.clj
@@ -1,6 +1,5 @@
 (ns gdl.tiled
-  (:require [gdl.graphics.batch :refer [batch]]
-            [gdl.graphics.world :as world])
+  (:require [gdl.graphics.world :as world])
   (:import [com.badlogic.gdx.maps MapRenderer MapLayer MapLayers MapProperties]
            [com.badlogic.gdx.maps.tiled TmxMapLoader TiledMap TiledMapTile
             TiledMapTileLayer TiledMapTileLayer$Cell]
@@ -100,7 +99,7 @@
 ; OrthogonalTiledMapRenderer extends BatchTiledMapRenderer
 ; and when a batch is passed to the constructor
 ; we do not need to dispose the renderer
-(defn- map-renderer-for [tiled-map color-setter]
+(defn- map-renderer-for [batch tiled-map color-setter]
   (OrthogonalTiledMapRendererWithColorSetter. tiled-map
                                               (float world/unit-scale)
                                               batch
@@ -110,8 +109,8 @@
 
 (def ^:private cached-map-renderer (memoize map-renderer-for))
 
-(defn render-map [tiled-map color-setter]
-  (let [^MapRenderer map-renderer (cached-map-renderer tiled-map color-setter)]
+(defn render-map [batch tiled-map color-setter]
+  (let [^MapRenderer map-renderer (cached-map-renderer batch tiled-map color-setter)]
     (world/update-camera-position)
     (.setView map-renderer world/camera)
     (->> tiled-map
