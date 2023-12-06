caef314d01b4bcc6f0adebd69cc69214d715c2f2
Michael Sappler <michaelsappler@gmail.com>
2023-12-06 20:08:51 +0100

    Remove global var gdl.graphics.unit-scale/*unit-scale*

--- src/gdl/render.clj
+++ src/gdl/render.clj
@@ -1,14 +1,12 @@
 (ns gdl.render
-  (:require [gdl.graphics.unit-scale :refer [*unit-scale*]]
-            [gdl.graphics.batch :refer [batch]]
+  (:require [gdl.graphics.batch :refer [batch]]
             [gdl.graphics.shape-drawer :as shape-drawer])
   (:import (com.badlogic.gdx.graphics Color OrthographicCamera)))
 
 (defn render-with [unit-scale ^OrthographicCamera camera renderfn]
-  (binding [*unit-scale* unit-scale]
-    (shape-drawer/set-line-width *unit-scale*)
-    (.setColor batch Color/WHITE) ; fix scene2d.ui.tooltip flickering
-    (.setProjectionMatrix batch (.combined camera))
-    (.begin batch)
-    (renderfn)
-    (.end batch)))
+  (shape-drawer/set-line-width unit-scale)
+  (.setColor batch Color/WHITE) ; fix scene2d.ui.tooltip flickering
+  (.setProjectionMatrix batch (.combined camera))
+  (.begin batch)
+  (renderfn unit-scale)
+  (.end batch))
